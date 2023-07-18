8d224a33a2864c1ba34ffd28e94279aaba498a4e
indhirapandiyan <indhirapandiyan2315@gmail.com>
2023-07-18 16:43:09 +0530

    dom

---
+++ day 14/script.js
@@ -0,0 +1,51 @@
+// console.log(window);
+// console.log(window.location);
+// console.log(window.location.href);
+// console.log(window.location.hostname);
+// console.log(window.location.protocol);
+// window.localStorage.setItem('userName',"pandi");
+
+// console.log(window.localStorage.getItem('userName'));
+
+// let sayHello=()=>{
+//     console.log('after five seconds');
+// };
+// window.setTimeout(sayHello,5000);
+
+// let sayHello=()=>{
+//     console.log('after five seconds');
+// };
+// window.setInterval(sayHello,1000);
+
+// function countDownTimer(seconds){
+
+//    if (seconds>0){
+
+//     console.log(seconds)
+//     setTimeout(()=> {
+//     countDownTimer(seconds -1)
+//     },1000);
+// }else{
+//     console.log('Happy New year');
+// }
+
+// }
+// countDownTimer(10);
+
+// let name=prompt('your name?');
+
+// console.log(name);
+
+// alert('hello');
+
+// let confirmAnswer=confirm('Are You Sure?');
+
+// if(confirmAnswer){
+//     console.log('Clicked Ok');
+// }else{
+//     console.log('Clicked Cancel');
+// }
+
+// console.log(window.innerWidth,window.innerHeight);
+
+// document.body.innerHTML="<h1>HELLO</h1>";
